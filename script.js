ee80f3e69f365c301cf7b742efaa36064e671239
R-Najdaoui <najdaoui.ra@gmail.com>
2025-11-02 11:12:14 +0100

    Add country flags next to city names

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const searchBtn = document.getElementById("searchBtn");
 const cityInput = document.getElementById("city");
 const weatherResult = document.getElementById("weatherResult");
 const API_KEY = "f3afd44bccfc21c8f16076f4876395f1";
+
 // click event listener
 searchBtn.addEventListener("click", () => {
     const city = cityInput.value.trim();
@@ -10,6 +11,7 @@ searchBtn.addEventListener("click", () => {
         weatherResult.textContent = "Please enter a city name!";
         return;
     }
+
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     fetch(url)
@@ -20,8 +22,16 @@ searchBtn.addEventListener("click", () => {
             return response.json();
         })
         .then(data => {
+            // flags
+            const countryCode = data.sys.country.toLowerCase();
+            const flagUrl = `https://flagcdn.com/48x36/${countryCode}.png`;
+
+            // display weather + flags
             weatherResult.innerHTML = `
-                <h2>${data.name}, ${data.sys.country}</h2>
+                <h2>
+                    ${data.name}, ${data.sys.country} 
+                    <img src="${flagUrl}" alt="${data.sys.country} flag">
+                </h2>
                 <p>Temperature: ${data.main.temp}°C</p>
                 <p>Weather: ${data.weather[0].description}</p>
                 <p>Humidity: ${data.main.humidity}%</p>
