8a6fad691cd9546ff378cb4477f4ce6a05a9b502
R-Najdaoui <najdaoui.ra@gmail.com>
2025-11-02 11:09:46 +0100

    Integrate OpenWeatherMap API to fetch and display real weather data

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const searchBtn = document.getElementById("searchBtn");
 const cityInput = document.getElementById("city");
 const weatherResult = document.getElementById("weatherResult");
-
+const API_KEY = "f3afd44bccfc21c8f16076f4876395f1";
 // click event listener
 searchBtn.addEventListener("click", () => {
     const city = cityInput.value.trim();
@@ -10,5 +10,24 @@ searchBtn.addEventListener("click", () => {
         weatherResult.textContent = "Please enter a city name!";
         return;
     }
-    weatherResult.textContent = `You searched for: ${city}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+            return response.json();
+        })
+        .then(data => {
+            weatherResult.innerHTML = `
+                <h2>${data.name}, ${data.sys.country}</h2>
+                <p>Temperature: ${data.main.temp}°C</p>
+                <p>Weather: ${data.weather[0].description}</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+            `;
+        })
+        .catch(error => {
+            weatherResult.textContent = error.message;
+        });
 });
